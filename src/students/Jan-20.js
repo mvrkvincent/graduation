8e6868a8cca148b4a8ed8a4dd46e9b27c06f675c
Mark Rodriguez <mrkxvncnt@gmail.com>
2020-07-10 13:25:17 -0400

    Add January students

---
+++ src/students/Jan-20.js
@@ -0,0 +1,94 @@
+const students = [
+  {
+    "id": 4723,
+    "name": "Abdullah Wafy",
+    "occup": "student"
+  },
+  {
+    "id": 4742,
+    "name": "Alan Li",
+    "occup": "student"
+  },
+  {
+    "id": 4733,
+    "name": "Brandon Tsui",
+    "occup": "student"
+  },
+  {
+    "id": 4740,
+    "name": "Chris Talley",
+    "occup": "student"
+  },
+  {
+    "id": 4730,
+    "name": "Drew Bartleman",
+    "occup": "student"
+  },
+  {
+    "id": 4726,
+    "name": "Ian Magenta",
+    "occup": "student"
+  },
+  {
+    "id": 4751,
+    "name": "Jesse Solis",
+    "occup": "student"
+  },
+  {
+    "id": 4717,
+    "name": "Joshua Cachola",
+    "occup": "student"
+  },
+  {
+    "id": 4755,
+    "name": "Lisa Kang",
+    "occup": "student"
+  },
+  {
+    "id": 4801,
+    "name": "Lizzie Friedman",
+    "occup": "student"
+  },
+  {
+    "id": 4731,
+    "name": "Mai Van Wagner",
+    "occup": "student"
+  },
+  {
+    "id": 4746,
+    "name": "Mark Mansolino",
+    "occup": "student"
+  },
+  {
+    "id": 4732,
+    "name": "Monia Techini",
+    "occup": "student"
+  },
+  {
+    "id": 4744,
+    "name": "Riki Kaneshiro",
+    "occup": "student"
+  },
+  {
+    "id": 4807,
+    "name": "Seamus Le",
+    "occup": "student"
+  },
+  {
+    "id": 4792,
+    "name": "Sean McLaughlan",
+    "occup": "student"
+  },
+  {
+    "id": 4729,
+    "name": "Will Schrader",
+    "occup": "student"
+  },
+  {
+    "id": 4711,
+    "name": "Zach Rizer",
+    "occup": "student"
+  }
+];
+
+export default students;
