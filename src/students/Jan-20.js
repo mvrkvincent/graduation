93138b2b86ee10826431c42ce7e8a5ab56317730
Mark Rodriguez <mrkxvncnt@gmail.com>
2020-07-10 13:44:16 -0400

    Update images

--- src/students/Jan-20.js
+++ src/students/Jan-20.js
@@ -2,91 +2,109 @@ const students = [
   {
     "id": 4723,
     "name": "Abdullah Wafy",
+    "imageUrl": "https://avatars2.githubusercontent.com/u/60306709?s=460&u=a94789211fd125f7e4e65a663170f7af36be58fc&v=4",
     "occup": "student"
   },
   {
     "id": 4742,
     "name": "Alan Li",
+    "imageUrl": "https://avatars0.githubusercontent.com/u/25993269?s=460&u=512d292f21e7b37e343288f93bd56ee6a2aec353&v=4",
     "occup": "student"
   },
   {
     "id": 4733,
     "name": "Brandon Tsui",
+    "imageUrl": "https://avatars2.githubusercontent.com/u/60237156?s=460&u=5e57f521c315ed6badfacd7ab8659543001b4032&v=4",
     "occup": "student"
   },
   {
     "id": 4740,
     "name": "Chris Talley",
+    "imageUrl": "https://avatars1.githubusercontent.com/u/58754802?s=460&u=2b7ba8aff5aa5d9faca12b17e7b78a5ad6c6f68c&v=4",
     "occup": "student"
   },
   {
     "id": 4730,
     "name": "Drew Bartleman",
+    "imageUrl": "https://avatars3.githubusercontent.com/u/25072600?s=460&u=12e8d99f08b8b25b7e7b27dcb40683e8e33ca8c2&v=4",
     "occup": "student"
   },
   {
     "id": 4726,
     "name": "Ian Magenta",
+    "imageUrl": "https://avatars0.githubusercontent.com/u/59981318?s=460&u=497c7d5ebe39d992358d51af1ebf50185d9d8f2d&v=4",
     "occup": "student"
   },
   {
     "id": 4751,
     "name": "Jesse Solis",
+    "imageUrl": "https://avatars2.githubusercontent.com/u/60208702?s=460&u=b6874977fb14e32c1441e2844643a970fd057a4c&v=4",
     "occup": "student"
   },
   {
     "id": 4717,
     "name": "Joshua Cachola",
+    "imageUrl": "",
     "occup": "student"
   },
   {
     "id": 4755,
     "name": "Lisa Kang",
+    "imageUrl": "https://avatars0.githubusercontent.com/u/58575516?s=460&u=aad8a111470d0a93bd9f159416b30bfd8b645410&v=4",
     "occup": "student"
   },
   {
     "id": 4801,
     "name": "Lizzie Friedman",
+    "imageUrl": "",
     "occup": "student"
   },
   {
     "id": 4731,
     "name": "Mai Van Wagner",
+    "imageUrl": "https://avatars1.githubusercontent.com/u/56708722?s=460&u=f4ce7242253f4f715a8b16667667f7c70ad6156b&v=4",
     "occup": "student"
   },
   {
     "id": 4746,
     "name": "Mark Mansolino",
+    "imageUrl": "",
     "occup": "student"
   },
   {
     "id": 4732,
     "name": "Monia Techini",
+    "imageUrl": "",
     "occup": "student"
   },
   {
     "id": 4744,
     "name": "Riki Kaneshiro",
+    "imageUrl": "https://avatars1.githubusercontent.com/u/60278879?s=460&u=3220928e50a1162ae18f396fe22bf321a798af21&v=4",
     "occup": "student"
   },
   {
     "id": 4807,
     "name": "Seamus Le",
+    "imageUrl": "https://avatars3.githubusercontent.com/u/60245279?s=460&u=ad283bc00b6ab7045fd337eae7519c75c67772bc&v=4",
     "occup": "student"
   },
   {
     "id": 4792,
     "name": "Sean McLaughlan",
+    "imageUrl": "https://avatars2.githubusercontent.com/u/59936991?s=460&u=fc8496dd8006fb54b817eef8ee9dcd92d58a1e55&v=4",
     "occup": "student"
   },
   {
     "id": 4729,
     "name": "Will Schrader",
+    "imageUrl": "https://avatars0.githubusercontent.com/u/17626612?s=460&u=b105eafeb69f24a28295901b7f374cdf12a7c4a2&v=4",
     "occup": "student"
   },
   {
     "id": 4711,
     "name": "Zach Rizer",
+    "imageUrl": "https://avatars3.githubusercontent.com/u/5158853?s=460&u=5d379282130f2f16ed60ef0b2adf1f99eed18105&v=4",
     "occup": "student"
   }
 ];
