22776e0010051c907a8aeb5d628e00d071f9e6a8
Mark Rodriguez <mrkxvncnt@gmail.com>
2020-08-04 22:10:05 -0400

    Feb 20 Cohort

--- src/students/Jan-20.js
+++ src/students/Jan-20.js
@@ -1,129 +1,47 @@
 const students = [
   {
-    "id": 4723,
-    "name": "Abdullah Wafy",
-    "imageUrl": "https://avatars2.githubusercontent.com/u/60306709?s=460&u=a94789211fd125f7e4e65a663170f7af36be58fc&v=4",
-    "occup": "student"
+    "name": "Johnny Bui",
+    "imageUrl": "https://avatars3.githubusercontent.com/u/60685723?s=460&u=8906bc06047d2374252904e393c4acb11358be6d&v=4",
   },
   {
-    "id": 4742,
-    "name": "Alan Li",
-    "imageUrl": "https://avatars0.githubusercontent.com/u/25993269?s=460&u=512d292f21e7b37e343288f93bd56ee6a2aec353&v=4",
-    "occup": "student"
+    "name": "Emily Burnham",
+    "imageUrl": "https://avatars2.githubusercontent.com/u/60051506?s=400&u=2c3816bbd476d8e274b942e2d6e2402533224ea3&v=4",
   },
   {
-    "id": 4742,
-    "name": "Alfredo Quiroga",
-    "imageUrl": "https://media-exp1.licdn.com/dms/image/C4D35AQEqP5U6W-i24A/profile-framedphoto-shrink_400_400/0?e=1594494000&v=beta&t=nfBsWc1yOF8X53R7iqqAjPFaDVDB-751IY1ZJPJ9Ggk",
-    "occup": "student"
+    "name": "Zach Henderson",
+    "imageUrl": "https://avatars0.githubusercontent.com/u/60163626?s=400&u=c764e1cc05a548b756cf5cd580ea33a13f3321e3&v=4",
   },
   {
-    "id": 4742,
-    "name": "Andras Szikszai",
-    "imageUrl": "https://media-exp1.licdn.com/dms/image/C4E03AQHHq1zRd1rf0w/profile-displayphoto-shrink_400_400/0?e=1599696000&v=beta&t=B1lohkzLjlmIk4L-VF9bLA1ci4o6CQ2U2b1UWYY7WI0",
-    "occup": "student"
+    "name": "Geoffrey Otieno",
+    "imageUrl": "https://avatars1.githubusercontent.com/u/50849936?s=460&u=d13d80da95ef7dc4a08721a1cb4bccf0da516677&v=4",
   },
   {
-    "id": 4733,
-    "name": "Brandon Tsui",
-    "imageUrl": "https://avatars2.githubusercontent.com/u/60237156?s=460&u=5e57f521c315ed6badfacd7ab8659543001b4032&v=4",
-    "occup": "student"
+    "name": "Ben Perlmutter",
+    "imageUrl": "https://avatars0.githubusercontent.com/u/57849986?s=460&u=8897665d835d70b9b506b44e525eebcf0de3053e&v=4",
   },
   {
-    "id": 4740,
-    "name": "Chris Talley",
-    "imageUrl": "https://avatars1.githubusercontent.com/u/58754802?s=460&u=2b7ba8aff5aa5d9faca12b17e7b78a5ad6c6f68c&v=4",
-    "occup": "student"
+    "name": "Aaron Pierskalla",
+    "imageUrl": "https://avatars0.githubusercontent.com/u/49695239?s=460&u=1a13dfb5d9f6a9972cc7cffce457d0280f21450c&v=4",
   },
   {
-    "id": 4730,
-    "name": "Drew Bartleman",
-    "imageUrl": "https://avatars3.githubusercontent.com/u/25072600?s=460&u=12e8d99f08b8b25b7e7b27dcb40683e8e33ca8c2&v=4",
-    "occup": "student"
+    "name": "James Robertson",
+    "imageUrl": "https://avatars0.githubusercontent.com/u/21094319?s=460&u=6afe5399b07695a628d0513293a9b183c17bdea9&v=4",
   },
   {
-    "id": 4726,
-    "name": "Ian Magenta",
-    "imageUrl": "https://avatars0.githubusercontent.com/u/59981318?s=460&u=497c7d5ebe39d992358d51af1ebf50185d9d8f2d&v=4",
-    "occup": "student"
+    "name": "Doug Ryu",
+    "imageUrl": "https://avatars0.githubusercontent.com/u/49464316?s=460&u=214eb91444d414c44262a05be1a216339beba96c&v=4",
   },
   {
-    "id": 4726,
-    "name": "James Ballard",
-    "imageUrl": "https://avatars2.githubusercontent.com/u/60041628?s=460&u=99c9fe9c1e76e64ade117a4451740b02c391b1c3&v=4",
-    "occup": "student"
+    "name": "Giancarlo Sanchez",
+    "imageUrl": "https://avatars2.githubusercontent.com/u/60968129?s=460&u=40626fc20a0de43bd1d1fd02c3cb2549e97c4109&v=4",
   },
   {
-    "id": 4751,
-    "name": "Jesse Solis",
-    "imageUrl": "https://avatars2.githubusercontent.com/u/60208702?s=460&u=b6874977fb14e32c1441e2844643a970fd057a4c&v=4",
-    "occup": "student"
+    "name": "Brad Simpson",
+    "imageUrl": "https://avatars0.githubusercontent.com/u/59807764?s=460&u=71ebbd7116b917764ca8770782f4b30e55369c13&v=4",
   },
   {
-    "id": 4717,
-    "name": "Joshua Cachola",
-    "imageUrl": "https://lutheryamaguchii.files.wordpress.com/2010/02/4258984607_4ed8c15293.jpg",
-    "occup": "student"
-  },
-  {
-    "id": 4755,
-    "name": "Lisa Kang",
-    "imageUrl": "https://avatars0.githubusercontent.com/u/58575516?s=460&u=aad8a111470d0a93bd9f159416b30bfd8b645410&v=4",
-    "occup": "student"
-  },
-  {
-    "id": 4801,
-    "name": "Lizzie Friedman",
-    "imageUrl": "https://lutheryamaguchii.files.wordpress.com/2010/02/4258984607_4ed8c15293.jpg",
-    "occup": "student"
-  },
-  {
-    "id": 4731,
-    "name": "Mai Van Wagner",
-    "imageUrl": "https://avatars1.githubusercontent.com/u/56708722?s=460&u=f4ce7242253f4f715a8b16667667f7c70ad6156b&v=4",
-    "occup": "student"
-  },
-  {
-    "id": 4746,
-    "name": "Mark Mansolino",
-    "imageUrl": "https://lutheryamaguchii.files.wordpress.com/2010/02/4258984607_4ed8c15293.jpg",
-    "occup": "student"
-  },
-  {
-    "id": 4732,
-    "name": "Monia Techini",
-    "imageUrl": "https://lutheryamaguchii.files.wordpress.com/2010/02/4258984607_4ed8c15293.jpg",
-    "occup": "student"
-  },
-  {
-    "id": 4744,
-    "name": "Riki Kaneshiro",
-    "imageUrl": "https://avatars1.githubusercontent.com/u/60278879?s=460&u=3220928e50a1162ae18f396fe22bf321a798af21&v=4",
-    "occup": "student"
-  },
-  {
-    "id": 4807,
-    "name": "Seamus Le",
-    "imageUrl": "https://avatars3.githubusercontent.com/u/60245279?s=460&u=ad283bc00b6ab7045fd337eae7519c75c67772bc&v=4",
-    "occup": "student"
-  },
-  {
-    "id": 4792,
-    "name": "Sean McLaughlan",
-    "imageUrl": "https://avatars2.githubusercontent.com/u/59936991?s=460&u=fc8496dd8006fb54b817eef8ee9dcd92d58a1e55&v=4",
-    "occup": "student"
-  },
-  {
-    "id": 4729,
-    "name": "Will Schrader",
-    "imageUrl": "https://avatars0.githubusercontent.com/u/17626612?s=460&u=b105eafeb69f24a28295901b7f374cdf12a7c4a2&v=4",
-    "occup": "student"
-  },
-  {
-    "id": 4711,
-    "name": "Zach Rizer",
-    "imageUrl": "https://avatars3.githubusercontent.com/u/5158853?s=460&u=5d379282130f2f16ed60ef0b2adf1f99eed18105&v=4",
-    "occup": "student"
+    "name": "Felipe Quiros",
+    "imageUrl": "https://avatars3.githubusercontent.com/u/59105242?s=460&u=1536418899475970d245b487edb30b1fba65ef09&v=4",
   }
 ];
 
