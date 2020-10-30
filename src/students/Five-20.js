9191bedf5b482d1b312b088cbd555056b4333cbd
Mark Vincent <mvrktopher@gmail.com>
2020-10-30 11:29:34 -0400

    Update May

---
+++ src/students/Five-20.js
@@ -0,0 +1,72 @@
+const students = [
+  {
+    "name": "Brandon Held",
+    "imageUrl": "https://avatars1.githubusercontent.com/u/44881685?s=460&u=9af981e530617367ebf469103c3ea05c8920aedf&v=4",
+  },
+  {
+    "name": "Caitlin Conway",
+    "imageUrl": "https://media-exp1.licdn.com/dms/image/C4E35AQFU0aoIMmJo8Q/profile-framedphoto-shrink_400_400/0?e=1604160000&v=beta&t=5lhHKVnXz_KFawsDFV5R2MncwYayx5UIM4sWFXu56sk",
+  },
+  {
+    "name": "Cole Hunter",
+    "imageUrl": "https://avatars3.githubusercontent.com/u/60110662?s=460&v=4",
+  },
+  {
+    "name": "Daniel Wilkins",
+    "imageUrl": "https://avatars0.githubusercontent.com/u/13189873?s=460&u=feaad44753ac68ca8087d94e02a9fff96b0c8146&v=4",
+  },
+  {
+    "name": "Dustin Schulte",
+    "imageUrl": "https://media-exp1.licdn.com/dms/image/C5603AQHD_QNK2JowFw/profile-displayphoto-shrink_400_400/0?e=1609372800&v=beta&t=Z9poQYkNzK2PWQEVNp3REZS5mzzA0laAfaItJlhY5ik",
+  },
+  {
+    "name": "Dylan Besonen",
+    "imageUrl": "https://avatars1.githubusercontent.com/u/64868310?s=460&u=fc3b016a7eef337312d7cbf2c6672c5abef3a2e8&v=4",
+  },
+  {
+    "name": "Tadeo Garcia",
+    "imageUrl": "https://avatars3.githubusercontent.com/u/65257152?s=460&u=0e48e7130857fc93d0ab7600b7c37e9eeb303857&v=4",
+  },
+  {
+    "name": "Lauren Beard",
+    "imageUrl": "https://media-exp1.licdn.com/dms/image/C5603AQHXTjZHD3W_JQ/profile-displayphoto-shrink_400_400/0?e=1609372800&v=beta&t=9Y24Rw4kUJ8YK9dK8c8c3qy7mrJ93h9m32qECRyjzHU",
+  },
+  {
+    "name": "Matt Taylor",
+    "imageUrl": "https://avatars1.githubusercontent.com/u/64988940?s=460&v=4",
+  },
+  {
+    "name": "Nick Mallas",
+    "imageUrl": "https://avatars0.githubusercontent.com/u/65270779?s=460&u=5eb1611e3fc286635beccea9d65c635885f6f28e&v=4",
+  },
+  {
+    "name": "Nick Litz",
+    "imageUrl": "https://avatars3.githubusercontent.com/u/43977856?s=460&u=28514e238460d547ccb21fc946e585671cb90f17&v=4",
+  },
+  {
+    "name": "Nish Arya",
+    "imageUrl": "https://avatars0.githubusercontent.com/u/64811465?s=460&u=ab15a7552476ecfa914369adb894a687ef5e79fb&v=4",
+  },
+  {
+    "name": "Panayiotis Dimopoulos",
+    "imageUrl": "https://media-exp1.licdn.com/dms/image/C4D03AQGL6RkXiM31wA/profile-displayphoto-shrink_400_400/0?e=1609372800&v=beta&t=dJuZsNClxWREs3SyPgX4kSARN6YQm_yDXLoj_HyeDoE",
+  },
+  {
+    "name": "Shaie Briggs",
+    "imageUrl": "https://avatars3.githubusercontent.com/u/63381315?s=460&u=dd05d59a759aadd900693f12498debc0ff5f29fa&v=4",
+  },
+  {
+    "name": "Simon Carroll",
+    "imageUrl": "https://avatars1.githubusercontent.com/u/65518099?s=460&u=ba428737aada60de2a131458192e0c78fc764b24&v=4",
+  },
+  {
+    "name": "Steven Tegnelia",
+    "imageUrl": "https://media-exp1.licdn.com/dms/image/C5603AQE4ucqgrhEQ3Q/profile-displayphoto-shrink_400_400/0?e=1609372800&v=beta&t=k1ia7vDt1gRo52vzVsyib-XWqzFKXiySeV3Jh7Xfoao",
+  },
+  {
+    "name": "Will Maccarty",
+    "imageUrl": "https://avatars0.githubusercontent.com/u/63319881?s=460&u=dd1540b4f126889c9de47162818a678d4389d1d4&v=4",
+  }
+];
+
+export default students;
